fd8db8bc5a392f2ac587f47dd9005597fabb6c17
amit.kumar <amit.kumar@1rstwap.com>
2019-04-08 17:42:40 +0400

    commnets added

--- src/main/java/com/ub/email/service/EmailService.java
+++ src/main/java/com/ub/email/service/EmailService.java
@@ -2,6 +2,16 @@ package com.ub.email.service;
 
 import com.ub.email.entity.EmailStats;
 
+/**
+ *  That Interface expose email functionality for email campaign
+ */
 public interface EmailService {
+
+    /**
+     * provides functionality to send email based on template id
+     *
+     * @param templateId That could be any template id exist in DB
+     * @return returns email statistic like total clicked, opened and delivered email
+     */
     EmailStats sendEmail(Long templateId);
 }
